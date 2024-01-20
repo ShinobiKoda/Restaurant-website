dd13572bc1b02d5281ad680bfb65147389003496
Praise <sirp2804@gmail.com>
2024-01-20 12:04:18 +0100

    completed

--- assets/script.js
+++ assets/script.js
@@ -30,7 +30,8 @@ const sections = document.querySelectorAll('.section[id]');
 
 
 function scrollActive(){
-   const scrollY = window.pageYOffset
+   const scrollY = window.scrollY;
+   const home =  document.querySelector('.home')
 
    sections.forEach(current=>{
       const sectionHeight = current.offsetHeight;
@@ -38,9 +39,11 @@ function scrollActive(){
       const sectionId =  current.getAttribute('id');
 
       if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-         document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link') ;
+         document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
+         
       }else{
          document.querySelector('.nav_menu a[href*=' + sectionId + "]").classList.remove('active-link');
+         home.classList.remove('active-link');
       }
    })
 }
@@ -67,6 +70,53 @@ function scrollTop() {
 
 window.addEventListener("scroll", scrollTop);
 
+// ===== DARK/LIGHT THEME ===== //
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme =  'bx-sun';
+
+// previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// we obtain current theme
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark': 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon': 'bx-sun';
+
+// validate if the user chose a theme
+if(selectedTheme){
+   // if the validation is true we ask what the issue was to activate theme
+   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+   themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// activate or deactivate the theme manaully with the button
+themeButton.addEventListener('click', ()=>{
+   // add or remove dark/icon theme 
+   document.body.classList.toggle(darkTheme);
+   themeButton.classList.toggle(iconTheme);
+
+   // save the theme that the user chose
+   localStorage.setItem('selected-theme', getCurrentTheme());
+   localStorage.setItem('selected-icon', getCurrentIcon());
+
+})
+
+//scroll reveal animation
+const sr = ScrollReveal({
+   origin: 'top',
+   distance: '30px',
+   duration: 2000,
+   reset: true
+});
+
+sr.reveal(`.home__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content, .app__data,
+            .app__img, .contact__data,
+            .contact__button, .footer__content`, {
+   interval: 200
+})
 
 
 
